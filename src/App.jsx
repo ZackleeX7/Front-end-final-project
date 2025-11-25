2e54e28401d1a71d58aa0a5c168fab83c1e5390b
ZackleeX7 <S22310026@student.unklab.ac.id>
2025-11-25 23:50:55 +0800

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import ProductSection from "./ProductSection";
-import FilterPanel from "./features/products/FilterPanel";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
-  <Header />
-  <main className="flex-grow container mx-auto p-6 space-y-6">
-    <h2 className="text-2xl font-semibold">
-      Selamat Datang di Inventori Produk
-    </h2>
-    <p className="text-gray-700">
-      Ini adalah aplikasi sederhana untuk mengelola daftar produk dengan fitur CRUD.
-    </p>
-    <ProductSection />
-  </main>
-  <Footer />
-</div>
+      <Header />
+      <main className="flex-grow container mx-auto p-6 space-y-6">
+        <h2 className="text-2xl font-semibold">
+          Selamat Datang di Inventori Produk
+        </h2>
+        <p className="text-gray-700">
+          Ini adalah aplikasi sederhana untuk mengelola daftar produk dengan fitur CRUD.
+        </p>
 
+        <ProductSection />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
